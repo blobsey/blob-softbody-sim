1f86189079a65ee2bedcc1c0855f65620516fb2f
blobsey <me@blobsey.com>
2026-02-08 01:25:11 -0800

    cursor

--- wasm/src/main.rs
+++ wasm/src/main.rs
@@ -10,16 +10,23 @@ async fn main() {
         x: initial_screen_width / 2.0,
         y: initial_screen_height / 2.0,
     });
+
+    let mut time: f32 = 0.0;
+
     loop {
+        let dt = get_frame_time().min(1.0 / 120.0);
+        time += dt;
+
         clear_background(WHITE);
         draw_text(&format!("FPS: {}", get_fps()), 0., 16., 32., BLACK);
-        let dt = get_frame_time().min(1.0 / 120.0);
 
         if is_mouse_button_down(MouseButton::Left) {
-            let blob_pos = blob.get_center_pos();
             let mouse_pos = Vec2::from(mouse_position());
+            let blob_pos = blob.get_center_pos();
             let direction = (mouse_pos - blob_pos) * dt * 0.075; // Scale it down
             blob.move_blob(direction);
+
+            draw_poly_lines(mouse_pos.x, mouse_pos.y, 3, 18.0, time * 200.0, 20.0, RED);
         }
 
         blob.update(get_frame_time().min(1.0 / 120.0));
